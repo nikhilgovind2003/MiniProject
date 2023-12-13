9bdc7714e6fa4ed24dc00a65086b4f57d4949214
Nikhil Govind <govindnikhil508@gmail.com>
2023-12-13 13:50:46 +0530

    mongodb added

--- server/index.js
+++ server/index.js
@@ -1,10 +1,27 @@
 import express from "express"
+import cors from "cors"
+import "dotenv/config"
+import mongoose from "mongoose"
+
+
 const app = express()
+app.use(cors())
 
 
+// ROUTES
 app.get("/", (req, res) => {
     res.json("hello world")
 })
 
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("Mongodb Connected successfully");
+    app.listen(4000,()=>{
+        console.log(`Server runs at the port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{console.log(err)})
 
-app.listen(4000)
+
+
+// 0fttJ3dCqWDTKAYa
